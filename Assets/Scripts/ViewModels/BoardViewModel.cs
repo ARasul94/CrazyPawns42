b27bc89cb67d7edd05fb5ce0d73bbdae659fe60c
Aliev Rasul <arasul110294@gmail.com>
2026-06-25 18:37:48 +0300

    refactor BoardViewModel.cs

--- Assets/Scripts/ViewModels/BoardViewModel.cs
+++ Assets/Scripts/ViewModels/BoardViewModel.cs
@@ -5,15 +5,23 @@ namespace ViewModels
 {
     public class BoardViewModel
     {
-        public ReactiveProperty<int> size { get; }
-        public ReactiveProperty<Color> blackCellColor { get; }
-        public ReactiveProperty<Color> whiteCellColor { get; }
+        public ReadOnlyReactiveProperty<int> size { get; }
+        public ReadOnlyReactiveProperty<Color> blackCellColor { get; }
+        public ReadOnlyReactiveProperty<Color> whiteCellColor { get; }
+        
+        private readonly ReactiveProperty<int> m_size;
+        private readonly ReactiveProperty<Color> m_blackCellColor;
+        private readonly ReactiveProperty<Color> m_whiteCellColor;
 
         public BoardViewModel(CrazyPawn.CrazyPawnSettings _settings)
         {
-            size = new ReactiveProperty<int>(_settings.CheckerboardSize);
-            blackCellColor = new ReactiveProperty<Color>(_settings.BlackCellColor);
-            whiteCellColor = new ReactiveProperty<Color>(_settings.WhiteCellColor);
+            m_size = new ReactiveProperty<int>(_settings.CheckerboardSize);
+            m_blackCellColor = new ReactiveProperty<Color>(_settings.BlackCellColor);
+            m_whiteCellColor = new ReactiveProperty<Color>(_settings.WhiteCellColor);
+            
+            size = new ReadOnlyReactiveProperty<int>(m_size);
+            blackCellColor = new ReadOnlyReactiveProperty<Color>(m_blackCellColor);
+            whiteCellColor = new ReadOnlyReactiveProperty<Color>(m_whiteCellColor);
         }
     }
 }
